91928c755a1ffb46b9ba86c31b050a4c0de0ef22
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2016-11-20 19:04:25 +0700

    react state example

--- App.jsx
+++ App.jsx
@@ -1,56 +1,19 @@
 import React from 'react';
 
 class App extends React.Component{
-	constructor(){
-		super();
-		this.state = {
-			data: [
-				{
-				'id': 1,
-				'name': 'Ipan Taupik Rahman',
-				'age': '20' 
-				},{
-					'id': 2,
-					'name': 'Zulkifli',
-					'age': '20'
-				},{
-					'id': 3,
-					'name': 'Ganjar',
-					'age': '20'
-				}]
+	constructor(props){
+		super(props);
+		this.state={
+			header: 'header this state....',
+			'content': 'Content from state'
 		}
 	}
 	render(){
 		return(
-		<div>
-			<Header/>
-			<table>
-			<tbody>
-				{this.state.data.map((person,i) => <TableRow key = {i} data = {person}/>)}
-			</tbody>
-			</table>
-		</div>
-		);
-	}
-}
-class Header extends React.Component{
-	render(){
-		return (
 			<div>
-			<h1>Header</h1>
+				<h1>{this.state.header}</h1>
+				<h2>{this.state.content}</h2>
 			</div>
-		);
-	}
-}
-
-class TableRow extends React.Component{
-	render(){
-		return(
-			<tr>
-				<td>{this.props.data.id}</td>
-				<td>{this.props.data.name}</td>
-				<td>{this.props.data.age}</td>
-			</tr>
 		);
 	}
 }
